5a3fe8f98a5c3ec7e95c5b8982d19d2581023baa
parwateshivam <shivamparvate61@gmail.com>
2026-05-12 14:51:10 +0530

    update dashboard

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const host = import.meta.env.VITE_API_URL;
+const host = import.meta.env.VITE_API_URL;
 
-const host = import.meta.env.VITE_API_URL_DEV;
+// const host = import.meta.env.VITE_API_URL_DEV;
 
 export const registerUser = async (endpoint, data) => {
     try {
